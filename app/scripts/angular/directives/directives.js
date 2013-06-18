af8faf38727bb9d976d2507a06da581954ac54ae
Micah Godbolt <micahgodbolt@gmail.com>
2013-06-18 11:34:07 -0700

    removed responsive nav and used simple angular directive instead

--- app/scripts/angular/directives/directives.js
+++ app/scripts/angular/directives/directives.js
@@ -78,25 +78,5 @@ angular.module('ngProtobotsApp')
     },
   }
 })
-  .directive('responsiveNav', function() {
-    return {
-        // Restrict it to be an attribute in this case
-        restrict: 'A',
-        // responsible for registering DOM listeners as well as updating the DOM
-        link: function(scope, element, attrs) {
-            var navigation = responsiveNav(attrs.id, {
-                animate: true,        // Boolean: Use CSS3 transitions, true or false
-                transition: 400,      // Integer: Speed of the transition, in milliseconds
-                label: "Menu",        // String: Label for the navigation toggle
-                insert: "after",      // String: Insert the toggle before or after the navigation
-                customToggle: "",     // Selector: Specify the ID of a custom toggle
-                openPos: "relative",  // String: Position of the opened nav, relative or static
-                jsClass: "js",        // String: 'JS enabled' class which is added to <html> el
-                init: function(){},   // Function: Init callback
-                open: function(){},   // Function: Open callback
-                close: function(){}   // Function: Close callback
-              });
-        }
-    };
-});
+
 
